07fa751f1799093640ea970fb3fee1db43a50d4c
InfernalDescent <matilda.bowling16@gmail.com>
2025-11-05 19:43:57 -0500

    Add server functionality using Node.js, Express, and Nodemon.

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
