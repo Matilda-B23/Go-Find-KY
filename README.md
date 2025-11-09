fe95e2dd5048dce39e27abde7329ae0e66f0473d
InfernalDescent <matilda.bowling16@gmail.com>
2025-11-08 20:50:10 -0500

    Added backend functionality to get and filter data from the API.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Go-Find-KY
 A web app demonstrating knowledge and skill. Used to log sightings of animals or birds. 
-<<<<<<< HEAD
 Credit to Josh Comeau for the basic CSS reset used. (https://www.joshwcomeau.com/css/custom-css-reset)
-=======
->>>>>>> 72142dfede6aa195675ce64d682ab9aef7e0d136
--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.get('/api/birds', async (req, res) => {
+  const endpoint = 'https://nuthatch.lastelm.software/v2/birds?page=1&pageSize=100&region=North%20America&hasImg=true&operator=AND'
+  const response = await fetch(endpoint, {
+    headers: {
+      'API-Key': process.env.API_KEY } 
+  });
+  const allBirds = await response.json();
+ const filteredBirds = allBirds.entities.map(bird => ({
+    name: bird.name,
+    status: bird.status,
+    images: bird.images && bird.images.length > 0 ? [bird.images[0]] : ["No Image Available. "],
+    recordings: bird.recordings && bird.recordings.length >0 ? [bird.recordings[0].file] : ["No Recording Available."]
+  }));
+  res.json(filteredBirds); 
+});
+
+  app.use(express.static('public'));
 
 
 app.listen(PORT, () => {
